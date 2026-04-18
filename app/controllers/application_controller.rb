714f1c5dd8854575983c39b26ea8fe91b86d6278
Funky_JAP <137175292+FunkyJAP@users.noreply.github.com>
2026-04-18 03:37:23 +0000

    upgrade for hola mundo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
   def hello
-    render html: '¡Hola, mundo!'
+    render html: '¡Hola, mundo! ¡Muchas gracias!'
   end
 
   def goodbye
-    render html: '左様なら'
+    render html: 'render goodbyeから左様なら'
   end
 end
